154039969192d08ae0a204af2ab31588bd92ab97
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2026-01-17 12:17:53 +0100

    Delete NaveIOT directory

--- NaveIOT/DA/simulaiotdata.py
+++
@@ -1,8 +0,0 @@
-import json
-import misurazione
-
-
-#controllo per verificare che questo script funzioni se eseguito come principale e NON come modulo importato
-if __name__ == "__main__":
-    print("Questa è solo una prova (questo messaggio verrà rimosso non appena inizierà la scrittura del codice)") 
-    
