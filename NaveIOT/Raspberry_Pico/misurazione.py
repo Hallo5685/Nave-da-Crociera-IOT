14da624e5ed22e6c746c510024ca5ff61c5de9f2
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2026-02-28 15:15:49 +0100

    Update misurazione.py

--- NaveIOT/Raspberry_Pico/misurazione.py
+++ NaveIOT/Raspberry_Pico/misurazione.py
@@ -13,22 +13,34 @@ import dht
 #
 def leggi_temp(decimale): 
     
+    # definizione pin collegato al segnale del sensore
     PIN_SEGNALE = 0
     
-    sensor = dht.DHT11(Pin(PIN_SEGNALE))      # Sensore celeste
-    #sensor = dht.DHT22(Pin(N_PIN))     # Sensore bianco
-    #
+    # inizializzazione sensore DHT11 collegato al pin scelto
+    sensor = dht.DHT11(Pin(PIN_SEGNALE))
+    
+    # se si utilizza il DHT22 decommentare la riga sotto
+    #sensor = dht.DHT22(Pin(N_PIN))
+    
     try:
+        # avvia la misurazione del sensore
         sensor.measure()
 
+        # lettura temperatura e umidità
         temperatura = sensor.temperature()
         umidita = sensor.humidity()
+
+        # formattazione dei valori con numero di decimali scelto
         temp = f"{temperatura:.{decimale}f}"
         hum  = f"{umidita:.{decimale}f}"
     
     except OSError as e:
+        # errore di comunicazione con il sensore
         print('Problemi con il sensore...')
     
+    # assegno i valori formattati a variabili di ritorno
     TEMP = temp
     UMI  = hum
+
+    # ritorna temperatura e umidità come stringhe formattate
     return TEMP,UMI
